7d5816cd581ea006b41585a23b714a281f6bf486
Ayesh Ruwandeniya <ayesh.oracle@gmail.com>
2024-08-25 15:23:24 +0400

    loading gif

--- js/navigation.js
+++ js/navigation.js
@@ -25,20 +25,6 @@ function logout() {
   window.location.href = "/login.html";
 }
 
-function naviLoading() {
-  $("body").animate({ scrollTop: 0 }, 0);
-  $("body").after('<div id="DarkWorld"></div>');
-  $("body").before(
-    '<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>'
-  );
-  $(".spinner").center();
-}
-
-function naviLoadingHide() {
-  $("#DarkWorld").remove();
-  $(".spinner").remove();
-}
-
 /* function sessionExpired()
 {
   naviLoadingHide();
@@ -60,22 +46,3 @@ function naviLoadingHide() {
     }
   });
 } */
-
-jQuery.fn.center = function () {
-  this.css("position", "fixed");
-  this.css(
-    "top",
-    Math.max(
-      0,
-      ($(window).height() - $(this).outerHeight()) / 2 + $(window).scrollTop()
-    ) + "px"
-  );
-  this.css(
-    "left",
-    Math.max(
-      0,
-      ($(window).width() - $(this).outerWidth()) / 2 + $(window).scrollLeft()
-    ) + "px"
-  );
-  return this;
-};
