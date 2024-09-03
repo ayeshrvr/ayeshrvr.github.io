65e07bdaeeb5f32552b2be6cea41d94a41507d35
Ayesh Ruwandeniya <ayesh.oracle@gmail.com>
2024-09-04 01:59:19 +0400

    Updates

--- js/navigation.js
+++ js/navigation.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
   } else {
     const storedData = localStorage.getItem("userData");
     const userData = JSON.parse(storedData);
+    createSideNav();
     $("#uName").text(userData.Name);
     $("#uDesig").text(userData.Designation);
   }
@@ -24,6 +25,42 @@ function logout() {
   localStorage.removeItem("userData");
   localStorage.removeItem("isLoggedIn");
   window.location.href = "/login.html";
+}
+
+function createSideNav(){
+var htmlText = '<div>'+
+      '<li><a id="uName" class="subheader" style="color: #FF8816;"></a></li>'+
+      '<li><a id="uDesig" class="subheader" style="margin-top: -25px; font-size: 11px;"></a></li>'+
+  '</div>'+
+  '<li><a href="/" class="waves-effect">'+
+      '<i class="material-icons">home</i>Home</a>'+
+  '</li>'+
+  '<li><a href="/" class="waves-effect">'+
+      '<i class="material-icons">date_range</i>Attendance</a>'+
+  '</li>'+
+  '<li><a href="/runningchart.html" class="waves-effect">'+
+      '<i class="material-icons">directions_car</i>Running Chart</a>'+
+  '</li>'+
+  '<li><a href="/expenditures.html" class="waves-effect">'+
+      '<i class="material-icons">monetization_on</i>Expenditures</a>'+
+  '</li>'+
+  '<li><a href="/" class="waves-effect">'+
+      '<i class="material-icons">straighten</i>Project Progress</a>'+
+  '</li>'+
+  '<li>'+
+      '<div class="divider"></div>'+
+  '</li>'+
+  '<li><a href="/" class="waves-effect">'+
+          '<i class="material-icons">person_pin</i>Profile</a>'+
+  '</li>'+
+  '<li><a href="/" class="waves-effect">'+
+      '<i class="material-icons">info</i>About</a>'+
+  '</li>'+
+  '<li><a id="logOut" href="#" class="waves-effect">'+
+      '<i class="material-icons">lock</i>Logout</a>'+
+  '</li>';
+  
+  $("#side-menu").html(htmlText);
 }
 
 /* function sessionExpired()
