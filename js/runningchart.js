9ae5262df7c62ade123fdbffe6653f088c3a38f6
Ayesh Ruwandeniya <ayesh.oracle@gmail.com>
2024-09-04 14:35:02 +0400

    Update runningchart.js

--- js/runningchart.js
+++ js/runningchart.js
@@ -1,7 +1,35 @@
-$(document).ready(function(){
-    
-    $("#btnAddRC").click(function(){
-        alert("test");
-    });
-
+$(document).ready(function () {
+  $("#btnAddRC").click(function () {
+    addRecord();
+  });
 });
+
+function addRecord() {
+  //naviLoading();
+
+  const sheetDataHandler = (sheetData) => {
+    /* if (sheetData.length > 0) {
+      const serializedData = JSON.stringify(sheetData[0]);
+      localStorage.setItem("userData", serializedData);
+      localStorage.setItem("isLoggedIn", true);
+      window.location.href = "/index.html";
+    } else {
+      logError();
+    } */
+  };
+
+  getSheetData({
+    // sheetID you can find in the URL of your spreadsheet after "spreadsheet/d/"
+    sheetID: "1_eUwXCDKhnNw12IklJdkbZDcvF2Gg5UeH_lI09wKg8c",
+    // sheetName is the name of the TAB in your spreadsheet (default is "Sheet1")
+    sheetName: "tblRunningChart",
+    writeData: {
+        range: 'A:B', // Append data to the end of the sheet
+        data: [
+          ['NewUsername', 'NewPassword']
+        ]
+      }
+  });
+
+  return false;
+}
